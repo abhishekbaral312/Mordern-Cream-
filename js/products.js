10b6b06f595217bf9a82515b5ea863e9f1bd628f
Abhishek Baral <81559329+abhishekbaral312@users.noreply.github.com>
2025-06-30 12:33:46 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
   //product fetching from JSON file
-  fetch('product-list.json')
+  fetch('../js/product-list.json')
     .then(res => res.json())
     .then(products => {
       const list = document.getElementById('product-list');
