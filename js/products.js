161298760b6f03e654890380922c82798a9675c4
Abhishek Baral <81559329+abhishekbaral312@users.noreply.github.com>
2025-06-30 12:53:27 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,3 +1,12 @@
+window.addEventListener("load", function () {
+    const loader = document.getElementById("preloader");
+    if (loader) {
+      loader.style.opacity = "0";
+      loader.style.pointerEvents = "none";
+      setTimeout(() => loader.remove(), 500); // optional fade-out
+    }
+  });
+
 window.addEventListener('DOMContentLoaded', () => {
 
 
