c90a4eefb5e960793cdbd2ddb5b7cb5f2155d81a
Abhishek Baral <81559329+abhishekbaral312@users.noreply.github.com>
2025-06-30 12:53:10 +0530

    Update func.js

--- js/func.js
+++ js/func.js
@@ -1,8 +1,1 @@
-window.addEventListener("load", function () {
-    const loader = document.getElementById("preloader");
-    if (loader) {
-      loader.style.opacity = "0";
-      loader.style.pointerEvents = "none";
-      setTimeout(() => loader.remove(), 500); // optional fade-out
-    }
-  });
+
